3fb2e66250468a4cb74b266fbda1bb25f2181740
Yao Yuquan <55245305@qq.com>
2022-07-21 15:33:32 +0800

    leetcode problem3

---
+++ page1/problem3.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, text):
+        """
+        :type s: str
+        :rtype: int
+        """
+        store_map = {}
+        result, start, end = 0, 0, 0
+        while end < len(text):
+            if text[end] in store_map:
+                new_start = store_map[text[end]] + 1
+
+                for key in list(store_map):
+                    if store_map[key] < new_start:
+                        del store_map[key]
+
+                start = new_start
+
+            store_map[text[end]] = end
+            end = end + 1
+
+            result = max(result, end - start)
+
+        return result
